a382873013abdd98890bc86106bb05b9686268d0
Roger Trocio <rogertrocio29@gmail.com>
2025-06-02 20:15:35 +0800

    Add AuthTest for unit testing and  mysql_testing db connection

--- config/database.php
+++ config/database.php
@@ -112,6 +112,29 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        /**
+         * For unit testing database connection.
+         */
+        'mysql_testing' => [
+            'driver' => 'mysql',
+            'url' => env('TEST_DB_URL'),
+            'host' => env('TEST_DB_HOST', '127.0.0.1'),
+            'port' => env('TEST_DB_PORT', '3306'),
+            'database' => env('TEST_DB_DATABASE', 'laravel'),
+            'username' => env('TEST_DB_USERNAME', 'root'),
+            'password' => env('TEST_DB_PASSWORD', ''),
+            'unix_socket' => env('TEST_DB_SOCKET', ''),
+            'charset' => env('TEST_DB_CHARSET', 'utf8mb4'),
+            'collation' => env('TEST_DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
     ],
 
     /*
@@ -147,7 +170,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test for user log in with correct credentials.
+     *
+     * @return void
+     */
+    public function test_login_with_valid_credentials(): void
+    {
+        $user = User::factory()->admin()->create();
+
+        $response = $this->postJson('/authenticate', [
+            'email' => 'johndoe@eis.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(302)
+            ->assertRedirect('/');
+
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /**
+     * Test for user log in with incorrect credentials.
+     *
+     * @return void
+     */
+    public function test_login_with_invalid_credentials(): void
+    {
+        $response = $this->postJson('/authenticate', [
+            'email' => 'johndoe@eis.com',
+            'password' => 'passworddd',
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJson([
+                'errors' => [
+                    'email' => ['The provided credentials do not match our records.'],
+                ]
+            ]);
+
+        $this->assertGuest();
+    }
+
+    /**
+     * Test authenticated user to logged out.
+     *
+     * @return void
+     */
+    public function test_authenticated_user_can_logout(): void
+    {
+        $user = User::factory()->admin()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->postJson('/logout');
+
+        $response->assertStatus(302)
+            ->assertRedirect('/login');
+
+        $this->assertGuest();
+    }
+}
